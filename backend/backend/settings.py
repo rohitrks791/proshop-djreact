c2541742d5ecea5ff8edcf714775d1cd2b5a10bc
rohitrkx <rohitsinghx677@gmail.com>
2021-11-17 19:04:49 +0530

    changed settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -174,7 +174,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 REACT_APP_PATH = os.path.join(BASE_DIR, 'frontend')
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/images/'

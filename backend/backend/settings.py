5b62214dfd0ac7bc85ad1e5bc3e64149badb3a76
rohitrkx <rohitsinghx677@gmail.com>
2021-11-17 08:22:03 +0530

    changed some files

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -87,10 +87,10 @@ SIMPLE_JWT = {
 }
 
 MIDDLEWARE = [
-     "corsheaders.middleware.CorsMiddleware",
-
+    "corsheaders.middleware.CorsMiddleware",
 
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
